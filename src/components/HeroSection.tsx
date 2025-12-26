12a896f456a8006a134205a37855385784ffe9b2
yejun6891 <wns03919@naver.com>
2025-12-26 22:30:44 +0900

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,7 +13,6 @@ const slides = [
     title: "E-UM Mobility Innovation Research Institute",
     description: "이음모빌리티혁신연구소는 스마트 모빌리티 기술 개발과 연구를 통해",
   },
-  {
     description: "모두를 위한 이동권을 실현하고, 사회적 가치를 창출합니다.",
   },
 ];
