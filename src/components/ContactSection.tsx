615f6bafb9b8f9319009b18f1ab6d3a3f44b0e8e
강건희 <alex7576891@gmail.com>
2026-01-09 09:23:53 +0900

    지도 수정 테스트

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -13,7 +13,7 @@ export const ContactSection = () => {
           {/* Map */}
           <div className="rounded-lg overflow-hidden h-[400px] border">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3262.5807698!2d129.12888!3d35.16937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3568eb2f8c7dd9c9%3A0x3e8d7f5d5d5d5d5d!2z67aA7IKw6rSR7Jet7IucIOyVoOyatOuMgOq1rCDshLzthoTshJzroZwgMzA!5e0!3m2!1sko!2skr!4v1234567890123!5m2!1sko!2skr"
+              src="https://www.google.com/maps/place/%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C+%ED%95%B4%EC%9A%B4%EB%8C%80%EA%B5%AC+APEC%EB%A1%9C+17/data=!3m1!4b1!4m6!3m5!1s0x356892c796ffda13:0xe97d24f278635b12!8m2!3d35.1659653!4d129.1323746!16s%2Fg%2F11bzj3lgh_?entry=ttu&g_ep=EgoyMDI2MDEwNi4wIKXMDSoASAFQAw%3D%3D"
               width="100%"
               height="100%"
               style={{ border: 0 }}
